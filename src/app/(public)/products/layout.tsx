58b5a8476ff434b8f4d69b0f350fcc74c69dcbba
augustomatosdev <dmatosconsult@gmail.com>
2025-11-12 14:25:28 +0100

    first commit 3

---
+++ src/app/(public)/products/layout.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React, { Suspense } from "react";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <div>{children}</div>
+    </Suspense>
+  );
+};
+
+export default Layout;
